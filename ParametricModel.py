e969f04e8fb223e72cab2dcab1ddedf09f59ff58
Henry T <41020770+hmtessier@users.noreply.github.com>
2018-09-09 17:59:27 -0400

    Add files via upload

--- ParametricModel.py
+++ ParametricModel.py
@@ -7,10 +7,12 @@ Created on Sun Sep  9 15:17:41 2018
 
 import pandas as pd
 import statsmodels.api as sm
+import numpy as np
 
 df = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
 
+#code all categorical variables
 cols = df.columns[df.dtypes.eq('object')]
 for col in df[cols]:
     df[col] = df[col].astype('category')
@@ -22,13 +24,23 @@ for col in test[cols]:
     test[col] = test[col].cat.codes
 test = test.fillna(test.mean())
 
+#create initial model with all variables
+model1 = sm.OLS(df.SalePrice, df.iloc[:,1:80]).fit()
 
-model = sm.OLS(df.SalePrice, df.iloc[:,1:80]).fit()
+#potential vars to remove: 
+#LotConfig, YearRemodAdd, Foundation, CentralAir, BsmtHalfBath, HalfBath, GarageCond, EnclosedPorch, Fence, MiscVal, MoSold 
 
-pred = model.predict(test.iloc[:,1:])
+#create list of only significant variables
+significants = np.setdiff1d(df.iloc[:,1:80].columns, ['LotConfig','YearRemodAdd','Foundation','CentralAir','BsmtHalfBath','HalfBath','GarageCond','EnclosedPorch','Fence','MiscVal','MoSold'])
 
+#create new model using only variables from list
+model2 = sm.OLS(df.SalePrice, df[significants]).fit()
+
+#predict using new model
+pred = model2.predict(test[significants])
+
+#output to df and csv
 out = pd.DataFrame(columns = ['Id','SalePrice'])
 out['Id'] = test.Id
 out['SalePrice'] = pred
-
 out.to_csv('ModelOutput.csv',index=False)
